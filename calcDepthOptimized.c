6f844ca190508cce0d884229741e055a51d34b6a
Alex Khodaverdian <alexkhodaverdian@berkeley.edu>
2016-10-26 00:40:42 -0700

    Proj 4 release

---
+++ calcDepthOptimized.c
@@ -0,0 +1,24 @@
+// CS 61C Fall 2015 Project 4
+
+// include SSE intrinsics
+#if defined(_MSC_VER)
+#include <intrin.h>
+#elif defined(__GNUC__) && (defined(__x86_64__) || defined(__i386__))
+#include <x86intrin.h>
+#endif
+
+// include OpenMP
+#if !defined(_MSC_VER)
+#include <pthread.h>
+#endif
+#include <omp.h>
+
+#include "calcDepthOptimized.h"
+#include "calcDepthNaive.h"
+
+/* DO NOT CHANGE ANYTHING ABOVE THIS LINE. */
+
+void calcDepthOptimized(float *depth, float *left, float *right, int imageWidth, int imageHeight, int featureWidth, int featureHeight, int maximumDisplacement)
+{
+	calcDepthNaive(depth, left, right, imageWidth, imageHeight, featureWidth, featureHeight, maximumDisplacement);
+}
